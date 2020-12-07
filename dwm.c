fa62b13e7bf3bb047444d94fb4de5ca1d6b09ba8
Osama <iosamaify@gmail.com>
2020-12-07 23:26:38 +0200

    fix togglescratch declaration

--- dwm.c
+++ dwm.c
@@ -212,6 +212,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglescratch(const Arg *arg);
 static void togglesticky(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
