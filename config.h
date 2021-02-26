6d567f9fa723110f8cc9c3d4db73a938894b08b9
Osama <iosamaify@gmail.com>
2021-02-26 09:54:11 +0200

    update config

--- config.h
+++ config.h
@@ -45,18 +45,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,        NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,        NULL,       1 << 8,       0,           0,           -1 },
-	{ "Chromium", NULL,        NULL,       1 << 8,       0,           0,           -1 },
-	{ NULL,       "spterm",    NULL,       SPTAG(0),     0,           1,           -1 },
-	{ NULL,       NULL, "Picture in picture",  0,        0,           1,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,        NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,        NULL,       1 << 8,       0,           -1 },
+	{ "Chromium", NULL,        NULL,       1 << 8,       0,           -1 },
+	{ NULL,       "spterm",    NULL,       SPTAG(0),     1,           -1 },
+	{ NULL,       NULL, "Picture in picture",  0,        1,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
@@ -79,9 +79,9 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      comboview,      {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      combotag,       {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -106,9 +106,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,         incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_h,         setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             XK_l,         setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_semicolon, setcfact,       {.f =  0.00} },
 	{ MODKEY|ShiftMask,             XK_j,         movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,         movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_space,     zoom,           {0} },
@@ -137,6 +134,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,     tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,    tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_grave,     togglescratch,  {.ui = 0 } },
+	{ MODKEY|ShiftMask,             XK_q,         quit,           {0} },
 	TAGKEYS(                        XK_1,                         0)
 	TAGKEYS(                        XK_2,                         1)
 	TAGKEYS(                        XK_3,                         2)
@@ -180,7 +178,6 @@ static Key keys[] = {
 	/*
 	{ MODKEY,                       XK_space,     setlayout,      {0} },
 	{ MODKEY|ControlMask,           XK_space,     focusmaster,    {0} },
-	{ MODKEY|ShiftMask,             XK_q,         quit,           {0} },
 
 	{ MODKEY|Mod1Mask,              XK_u,         incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,         incrgaps,       {.i = -1 } },
@@ -203,13 +200,13 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	//{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	//{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        ShiftMask,      Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        defaultgaps,    {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
